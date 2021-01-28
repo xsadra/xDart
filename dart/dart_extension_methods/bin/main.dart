a0a1ab1da262117aa4da98f6feccdcdd9010d2cf
Sadra <31988724+xsadra@users.noreply.github.com>
2021-01-28 21:41:11 +0100

    String Extentions toEuro

--- dart/dart_extension_methods/bin/main.dart
+++ dart/dart_extension_methods/bin/main.dart
@@ -3,9 +3,14 @@ extension StringExtentions on String {
     final emailRegExp = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+\.[a-zA-Z]');
     return emailRegExp.hasMatch(this);
   }
+
+  String get toEuro {
+    return '$this€';
+  }
 }
 
 void main() {
   print("It's me!".isValidEmail);
   print('email@sadra.at'.isValidEmail);
+  print('10'.toEuro);
 }
