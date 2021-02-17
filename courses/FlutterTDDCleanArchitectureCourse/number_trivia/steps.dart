d0556c2fbc3d3678d53faf4e9dccb5dbb8732977
Sadra <31988724+xsadra@users.noreply.github.com>
2021-02-17 22:33:07 +0100

    create some certain packages to divide the app parts[write down the steps]

---
+++ courses/FlutterTDDCleanArchitectureCourse/number_trivia/steps.dart
@@ -0,0 +1,38 @@
+/**
+ * ++ : Create
+ * -- : Delete
+ * [] : package
+ * {} : class
+ * <> : abstract class
+ *
+ *
+ * Steps *******************************************
+ * ++[] features
+ * ++[] features.number_trivia
+ * ++[] features.number_trivia.data
+ * ++[] features.number_trivia.data.models
+ * ++[] features.number_trivia.data.datasources
+ * ++[] features.number_trivia.data.repositories
+ * ++[] features.number_trivia.domain
+ * ++[] features.number_trivia.domain.entities
+ * ++[] features.number_trivia.domain.usecases
+ * ++[] features.number_trivia.domain.repositories
+ * ++[] features.number_trivia.presentation
+ * ++[] features.number_trivia.presentation.bloc
+ * ++[] features.number_trivia.presentation.pages
+ * ++[] features.number_trivia.presentation.widgets
+ * ++[] core
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * */
