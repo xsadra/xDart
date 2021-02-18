c9c73e0f822601d0fe90d66f2c35ac79a216a6fe
Sadra <31988724+xsadra@users.noreply.github.com>
2021-02-18 17:28:12 +0100

    Create GetConcreteNumberTrivia class and import it to test

---
+++ courses/FlutterTDDCleanArchitectureCourse/number_trivia/lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+}
--- courses/FlutterTDDCleanArchitectureCourse/number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ courses/FlutterTDDCleanArchitectureCourse/number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
