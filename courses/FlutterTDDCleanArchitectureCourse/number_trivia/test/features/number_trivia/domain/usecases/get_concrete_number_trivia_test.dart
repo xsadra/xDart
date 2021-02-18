3e08f3bdf8575c0355a8bd36693c1b5ba1c638de
Sadra <31988724+xsadra@users.noreply.github.com>
2021-02-18 20:42:06 +0100

    refactor GetConcreteNumberTrivia class to use in-build call method instead of execute

--- courses/FlutterTDDCleanArchitectureCourse/number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ courses/FlutterTDDCleanArchitectureCourse/number_trivia/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -27,7 +27,7 @@ void main() {
       when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
           .thenAnswer((_) async => Right(tNumberTrivia));
       // act
-      final result = await useCase.execute(number: tNumber);
+      final result = await useCase(number: tNumber);
       // assert
       expect(result, Right(tNumberTrivia));
       verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
