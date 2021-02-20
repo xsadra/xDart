7b236e2055670084c38d23181d0b24f009270d8f
Sadra <31988724+xsadra@users.noreply.github.com>
2021-02-20 13:01:29 +0100

    Create NumberTriviaModel fromJson factory and test it for int and double

--- courses/FlutterTDDCleanArchitectureCourse/number_trivia/test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ courses/FlutterTDDCleanArchitectureCourse/number_trivia/test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Some text');
@@ -12,4 +16,31 @@ void main() {
       expect(tNumberTriviaModel, isA<NumberTrivia>());
     },
   );
+
+  group('fromJson', () {
+    test(
+      'should return a valid model when the JSON number is an Integer',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+    test(
+      'should return a valid model when the JSON number is an Double',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia_double.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+  });
 }
