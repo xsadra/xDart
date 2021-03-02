b612c615972514791084c1a23fd5d80a00e0a523
Sadra <31988724+xsadra@users.noreply.github.com>
2021-03-02 22:37:06 +0100

    Modeling the Sign-In Form Events & State [with BloC] using Union

---
+++ courses/FlutterFirebaseAndDDDCourse/notes/lib/application/auth/sign_in_form/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:meta/meta.dart';
+import 'package:notes/domain/auth/auth_failure.dart';
+import 'package:notes/domain/auth/i_auth_facade.dart';
+import 'package:notes/domain/auth/value_objects.dart';
+
+part 'sign_in_form_bloc.freezed.dart';
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+
+//? Step 9: add bloc to pubspec
+//? Step 10: Generate bloC using bloc generator
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade);
+
+  @override
+  SignInFormState get initialState => SignInFormState.initial();
+
+  @override
+  Stream<SignInFormState> mapEventToState(SignInFormEvent event) async* {
+    // TODO: Add your event logic
+  }
+}
