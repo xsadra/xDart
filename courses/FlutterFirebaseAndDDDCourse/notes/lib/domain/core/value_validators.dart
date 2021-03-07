a050984cd6b294e8f6c05d384098de8bf86dfbac
Sadra <31988724+xsadra@users.noreply.github.com>
2021-03-07 17:19:10 +0100

    Note Value Objects

--- courses/FlutterFirebaseAndDDDCourse/notes/lib/domain/core/value_validators.dart
+++ courses/FlutterFirebaseAndDDDCourse/notes/lib/domain/core/value_validators.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:kt_dart/kt.dart';
 import 'package:notes/domain/core/failures.dart';
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
@@ -13,4 +14,36 @@ Either<ValueFailure<String>, String> validatePassword(String input) {
   return input.length >= 6
       ? right(input)
       : left(ValueFailure.shortPassword(failureValue: input));
+}
+
+//? Step 42: add [Notes] validators to [value_validator] file
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength,
+) {
+  return input.length <= maxLength
+      ? right(input)
+      : left(ValueFailure.exceedingLength(failureValue: input, max: maxLength));
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  return input.isNotEmpty
+      ? right(input)
+      : left(ValueFailure.empty(failureValue: input));
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  // use ! to reverse the condition and return [right] first
+  return !input.contains('\n')
+      ? right(input)
+      : left(ValueFailure.multiline(failureValue: input));
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
+  KtList<T> input,
+  int maxLength,
+) {
+  return input.size <= maxLength
+      ? right(input)
+      : left(ValueFailure.listTooLong(failureValue: input, max: maxLength));
 }
---
+++ courses/FlutterFirebaseAndDDDCourse/notes/lib/domain/notes/value_objects.dart
@@ -0,0 +1,95 @@
+import 'dart:ui';
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:notes/domain/core/failures.dart';
+import 'package:notes/domain/core/value_objects.dart';
+import 'package:notes/domain/core/value_transformers.dart';
+import 'package:notes/domain/core/value_validators.dart';
+
+//? Step 43: create [NoteBody, TodoName, TodoColor, List3] entities
+//? Step 44: create [makeColorOpaque] to remove opacity from colors
+//? Step 45: use [KtList] instead of [List] from package [kt_dart]
+class NoteBody extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  static const maxLength = 1024;
+
+  factory NoteBody(String input) {
+    assert(input != null);
+    return NoteBody._(
+      // validateMaxStringLength(input, maxLength).flatMap(
+      //     (valueFromPreviousFunc) =>
+      //         validateStringNotEmpty(valueFromPreviousFunc)),
+      validateMaxStringLength(input, maxLength).flatMap(validateStringNotEmpty),
+    );
+  }
+
+  const NoteBody._(this.value);
+}
+
+class TodoName extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  static const maxLength = 30;
+
+  factory TodoName(String input) {
+    assert(input != null);
+    return TodoName._(
+      validateMaxStringLength(input, maxLength)
+          .flatMap(validateStringNotEmpty)
+          .flatMap(validateSingleLine),
+    );
+  }
+
+  const TodoName._(this.value);
+}
+
+class TodoColor extends ValueObject<Color> {
+  @override
+  final Either<ValueFailure<Color>, Color> value;
+  static const List<Color> predefinedColors = [
+    Colors.black45,
+    Colors.deepPurple,
+    Colors.red,
+    Colors.indigo,
+    Colors.indigoAccent,
+    Colors.green,
+    Colors.amber,
+    Colors.yellowAccent,
+  ];
+
+  factory TodoColor(Color input) {
+    assert(input != null);
+    return TodoColor._(
+      right(makeColorOpaque(input)),
+    );
+  }
+
+  const TodoColor._(this.value);
+}
+
+class List3<T> extends ValueObject<KtList<T>> {
+  @override
+  final Either<ValueFailure<KtList<T>>, KtList<T>> value;
+
+  static const maxLength = 3;
+
+  factory List3(KtList<T> input) {
+    assert(input != null);
+    return List3._(
+      validateMaxListLength(input, maxLength),
+    );
+  }
+
+  const List3._(this.value);
+
+  int get length {
+    return value.getOrElse(() => emptyList()).size;
+  }
+
+  bool get isFull => length == maxLength;
+}
