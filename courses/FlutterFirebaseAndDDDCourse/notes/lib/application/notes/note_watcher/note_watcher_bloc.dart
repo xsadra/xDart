604cc4c2b78a1371a7c13406d95e9edbd6c14dd0
Sadra <31988724+xsadra@users.noreply.github.com>
2021-03-11 17:38:13 +0100

    Note Watcher and Actor Blocs

---
+++ courses/FlutterFirebaseAndDDDCourse/notes/lib/application/notes/note_watcher/note_watcher_bloc.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:meta/meta.dart';
+import 'package:notes/domain/notes/i_note_repository.dart';
+import 'package:notes/domain/notes/note.dart';
+import 'package:notes/domain/notes/note_failure.dart';
+
+part 'note_watcher_bloc.freezed.dart';
+part 'note_watcher_event.dart';
+part 'note_watcher_state.dart';
+
+//? Step 52: generate [NoteWatcherBloc] bloC using bloc generator
+//? Step 53: configure [NoteWatcherEvent, NoteWatcherEvent] class
+@injectable
+class NoteWatcherBloc extends Bloc<NoteWatcherEvent, NoteWatcherState> {
+  final INoteRepository _noteRepository;
+
+  NoteWatcherBloc(this._noteRepository);
+
+  StreamSubscription<Either<NoteFailure, KtList<Note>>> _noteStreamSubscription;
+
+  @override
+  NoteWatcherState get initialState => const NoteWatcherState.initial();
+
+  @override
+  Stream<NoteWatcherState> mapEventToState(NoteWatcherEvent event) async* {
+    yield* event.map(
+      watchAllStarted: (e) async* {
+        yield const NoteWatcherState.loadInProgress();
+        await _noteStreamSubscription?.cancel();
+        _noteStreamSubscription = _noteRepository.watchAll().listen(
+              (failureOrNotes) =>
+                  add(NoteWatcherEvent.noteReceived(failureOrNotes)),
+            );
+      },
+      watchUncompletedStarted: (e) async* {
+        yield const NoteWatcherState.loadInProgress();
+        await _noteStreamSubscription?.cancel();
+        _noteStreamSubscription = _noteRepository.watchUncompleted().listen(
+              (failureOrNotes) =>
+                  add(NoteWatcherEvent.noteReceived(failureOrNotes)),
+            );
+      },
+      noteReceived: (e) async* {
+        yield e.failureOrNotes.fold(
+          (failure) => NoteWatcherState.loadFailure(failure),
+          (notes) => NoteWatcherState.loadSuccess(notes),
+        );
+      },
+    );
+  }
+
+  @override
+  Future<void> close() async {
+    await _noteStreamSubscription?.cancel();
+    return super.close();
+  }
+}
