049392c7247799424a7967ef9801da5591f25d7f
Sadra <31988724+xsadra@users.noreply.github.com>
2021-03-11 20:18:17 +0100

    Create, Update, Delete Notes

--- courses/FlutterFirebaseAndDDDCourse/notes/lib/domain/notes/note_failure.dart
+++ courses/FlutterFirebaseAndDDDCourse/notes/lib/domain/notes/note_failure.dart
@@ -4,8 +4,10 @@ part 'note_failure.freezed.dart';
 
 //? Step 49: create [NoteFailure]
 //? Step 57: add [InsufficientPermission] to [NoteFailure]
+//? Step 64: add [UnableToUpdate] to [NoteFailure]
 @freezed
 abstract class NoteFailure with _$NoteFailure {
   const factory NoteFailure.unexpected() = _Unexpected;
   const factory NoteFailure.insufficientPermission() = _InsufficientPermission;
+  const factory NoteFailure.unableToUpdate() = _UnableToUpdate;
 }
--- courses/FlutterFirebaseAndDDDCourse/notes/lib/infrastructure/core/firebase_injectable_module.dart
+++ courses/FlutterFirebaseAndDDDCourse/notes/lib/infrastructure/core/firebase_injectable_module.dart
@@ -1,13 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
 
 //? Step 24: create [FirebaseInjectableModule] to Registering third party types
-
 @module
 abstract class RegisterModule {
   @lazySingleton
   GoogleSignIn get googleSignIn => GoogleSignIn();
   @lazySingleton
   FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
+  @lazySingleton
+  Firestore get firestore => Firestore.instance;
 }
